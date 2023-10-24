45bee9cfb94092579ea6b32045f4e3694b318a51
el-sam <samurai.h@yahoo.com>
2023-10-24 21:16:02 +0800

    the findMatching method find the all mathing driver names regardless the case of he name, and return a new array that contains all driver names found matching.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+// Define function 
+function findMatching(drivers, name) {
+    
+    let newDrivers= drivers.filter(driverName => driverName.toUpperCase() === name.toUpperCase());
+
+
+    return newDrivers; 
+}
+
