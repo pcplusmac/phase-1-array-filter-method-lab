02ff6a37f33d1b54a296096e2a6602c713381acb
el-sam <samurai.h@yahoo.com>
2023-10-27 13:23:43 +0800

    inital commit on the this code

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ function findMatching(drivers, name) {
     return newDrivers; 
 }
 
+// Define function to find the drivers with specific names
+function fuzzyMatch(drivers, letter) {
+    
+    // let driverNew = []
+    let driver = (drivers.filter(driverName => driverName.substring(0,1) === letter
+    ));
+    // if (driver) {
+    //     driverNew = Object.assign({},driver)
+    // }
+    return driver;
+    // if (driver) {
+    //     newDrivers.push(driver);
+    //     return driverName;
+    // }
+     
+}
+
+function matchName (arr, str) {
+    let op = arr.filter (driver => driver.name === str)
+    for (let dr of op) {
+        return dr
+    }
+}
